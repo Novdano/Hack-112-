003b2957399bb78eb8d0b13bbafe5d4297a80a4e
Rishabh Chatterjee <rishabh.chatterjee98@gmail.com>
2016-11-13 13:30:02 -0500

    Update videosCardio.py

--- chooseFit/videosCardio.py
+++ chooseFit/videosCardio.py
@@ -1,7 +1,13 @@
 
-# Jogging             https://www.youtube.com/watch?v=C0rS43ocutI
-# Brisk Walk          https://www.youtube.com/watch?v=YLZhIQwt6L8
-# HIIT                https://www.youtube.com/watch?v=tbbZBtdd20U
-# Circuit Training    https://www.youtube.com/watch?v=460nSW7xEok
-# Rowing              https://www.youtube.com/watch?v=bc2bVqkkcDE
-# Zumba               https://www.youtube.com/watch?v=HRkNfdlm5Qs
+# Jogging               https://www.youtube.com/watch?v=C0rS43ocutI
+# Brisk Walk            https://www.youtube.com/watch?v=YLZhIQwt6L8
+# HIIT                  https://www.youtube.com/watch?v=tbbZBtdd20U
+# Circuit Training      https://www.youtube.com/watch?v=460nSW7xEok
+# Rowing                https://www.youtube.com/watch?v=bc2bVqkkcDE
+# Zumba                 https://www.youtube.com/watch?v=HRkNfdlm5Qs
+# Leg Press             https://www.youtube.com/watch?v=Aq5uxXrXq7c
+# Tricep Pushdown       https://www.youtube.com/watch?v=BLvSbziilmo
+# Hammer Curls          https://www.youtube.com/watch?v=IOOU5yChBhk
+# Fly                   https://www.youtube.com/watch?v=_4JjOqy0UiY
+# Shoulder Press        https://www.youtube.com/watch?v=qEwKCR5JCog
+# Bench Press           https://www.youtube.com/watch?v=6JtP6ju0IMw
