940f94fef4434f8b954d956db3707abd6e037468
Novdano <NovdanoDY@Novdanos-MBP.wv.cc.cmu.edu>
2016-11-12 23:58:17 -0500

    Added a lot of stuffs for the front end

--- chooseFit/finder/models.py
+++ chooseFit/finder/models.py
@@ -3,3 +3,17 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class userData(models.Model):
+    name = models.CharField(max_length = 100)
+    age = models.IntegerField()
+    weight = models.IntegerField() #pounds
+    heightFeet = models.IntegerField(default = 0) #inches
+    timeCommit = models.IntegerField() #hoursPerWeek
+    intro = models.TextField(default = "Hi")
+    goalChoices = ( ("bulking", "bulking"),
+                    ("slimming", "slimming"),
+                    ("fitness", "fitness" )
+                    )
+    goals = models.CharField(max_length = 10, choices =goalChoices)
+    heightInches = models.IntegerField(default = 0)
+    profile = models.CharField(max_length = 1000, default = "http://cdn3.denofgeek.us/sites/denofgeekus/files/scarlett_johansson.jpg")
--- chooseFit/finder/views.py
+++ chooseFit/finder/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from django.views.generic.base import View
+from django.http import HttpResponseRedirect
 
-# Create your views here.
+from .models import *
+
+class HomePage(View):
+    def get(self, request):
+        context = {}
+        context["posts"] = userData.objects.all()[::-1]
+        return render(request, 'specsForm.html', context)
+        
+class MakePost(View):
+    def post(self, request):
+        name = request.POST.get("Name")
+        age = request.POST.get("Age")
+        weight = request.POST.get("Weight")
+        Height_Feet = request.POST.get("Height (Feet)")
+        Height_Inches = request.POST.get("Height (Inches)")
+        timeCommitment = request.POST.get("Time Commitment")
+        #Introduction = request.POST.get("Introduction")
+        Goal = request.POST.get("Goal")
+        new_post = userData(name= name, age = age, weight = weight,
+                     heightFeet = Height_Feet, timeCommit = timeCommitment, goals =Goal, heightInches = Height_Inches)
+        new_post.save()
+        return HttpResponseRedirect("/")
+
+def recommended(request):
+    context = {}
+    context["posts"] = userData.objects.all()[::-1]
+    return render(request, 'recommended.html', context)
+        
