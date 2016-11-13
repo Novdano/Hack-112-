b68b845ade76dbb756f3ad88fa762154baf2c77f
Rishabh Chatterjee <rishabh.chatterjee98@gmail.com>
2016-11-13 13:25:08 -0500

    Add files via upload

---
+++ chooseFit/videosCardio.py
@@ -0,0 +1,7 @@
+
+# Jogging             https://www.youtube.com/watch?v=C0rS43ocutI
+# Brisk Walk          https://www.youtube.com/watch?v=YLZhIQwt6L8
+# HIIT                https://www.youtube.com/watch?v=tbbZBtdd20U
+# Circuit Training    https://www.youtube.com/watch?v=460nSW7xEok
+# Rowing              https://www.youtube.com/watch?v=bc2bVqkkcDE
+# Zumba               https://www.youtube.com/watch?v=HRkNfdlm5Qs
